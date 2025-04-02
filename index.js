53b33dced433a57cc60cb05875ad9d427cb044d5
caner <caner@Sevinces-iMac.local>
2025-04-02 21:04:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Gigachat - A peer-to-peer decentralized chat library
+ * @module gigachat
+ */
+
+const GigachatUser = require('./lib/core/user');
+const GigachatRoom = require('./lib/core/room');
+
+module.exports = {
+  GigachatUser,
+  GigachatRoom
+};
